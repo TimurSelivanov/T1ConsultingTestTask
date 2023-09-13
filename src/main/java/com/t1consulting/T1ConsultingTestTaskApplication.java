7553bcec29a238bbe7585f8e3aa79b2d386942ec
TimSelivan <timselivanson@gmail.com>
2023-09-13 18:46:56 +0300

    Init commit

---
+++ src/main/java/com/t1consulting/T1ConsultingTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.t1consulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1ConsultingTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1consulting/T1ConsultingTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1consulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ConsultingTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
