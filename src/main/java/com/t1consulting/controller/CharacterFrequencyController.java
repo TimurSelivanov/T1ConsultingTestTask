4410e510b8746a10e29aef84dbefd459d36078d5
TimSelivan <timselivanson@gmail.com>
2023-09-13 22:54:47 +0300

    Add method to process JSON request

--- src/main/java/com/t1consulting/controller/CharacterFrequencyController.java
+++ src/main/java/com/t1consulting/controller/CharacterFrequencyController.java
@@ -1,18 +1,26 @@
 package com.t1consulting.controller;
 
 import com.t1consulting.service.CharacterFrequencyService;
+import com.t1consulting.util.validation.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@Validated
 public class CharacterFrequencyController {
 
+    private final CharacterFrequencyService characterFrequencyService;
+
+
     @Autowired
-    private CharacterFrequencyService characterFrequencyService;
+    public CharacterFrequencyController(CharacterFrequencyService characterFrequencyService) {
+        this.characterFrequencyService = characterFrequencyService;
+    }
 
     //method to process HTTP request
     @GetMapping("/character-frequency")
@@ -20,11 +28,21 @@ public class CharacterFrequencyController {
         if (input == null || input.isEmpty()) {
             return ResponseEntity.badRequest().body("Input can not be empty");
         }
-        String result = characterFrequencyService.calculateCharacterFrequency(input);
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(characterFrequencyService.calculateCharacterFrequency(input));
     }
 
-    //TODO method to process JSON request
+    //method to process JSON request
+    @PostMapping("/character-frequency")
+    public ResponseEntity<?> getCharacterFrequencyJSON(@RequestBody(required = false) Map<String, String> request) {
+        if (request == null || !request.containsKey("input") || request.get("input").isEmpty()) {
+            ErrorMessage errorMessage = new ErrorMessage("Request can not be empty");
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        String input = request.get("input");
+
+        return ResponseEntity.ok(characterFrequencyService.calculateCharacterFrequencyMap(input));
+    }
 
 }
--- src/main/java/com/t1consulting/service/CharacterFrequencyService.java
+++ src/main/java/com/t1consulting/service/CharacterFrequencyService.java
@@ -28,4 +28,23 @@ public class CharacterFrequencyService {
 
         return sortedFrequencyMap.toString();
     }
+
+    public Map<Character, Integer> calculateCharacterFrequencyMap(String input) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+        for (char c : input.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        //sorting frequencyMap
+        LinkedHashMap<Character, Integer> sortedFrequencyMap = frequencyMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new
+                ));
+
+        return sortedFrequencyMap;
+    }
 }
