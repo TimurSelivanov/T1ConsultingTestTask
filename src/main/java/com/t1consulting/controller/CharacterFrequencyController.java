7245e864acb1ee57b47f90b4a012c5d74d6126e9
TimSelivan <timselivanson@gmail.com>
2023-09-13 19:01:43 +0300

    Create controller and service layers

---
+++ src/main/java/com/t1consulting/controller/CharacterFrequencyController.java
@@ -0,0 +1,10 @@
+package com.t1consulting.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CharacterFrequencyController {
+    //TODO Add methods to process requests
+}
---
+++ src/main/java/com/t1consulting/service/CharacterFrequencyService.java
@@ -0,0 +1,14 @@
+package com.t1consulting.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CharacterFrequencyService {
+
+    public Map<Character, Integer> calculateCharacterFrequency(String input) {
+        //TODO method logic
+        return null;
+    }
+}
