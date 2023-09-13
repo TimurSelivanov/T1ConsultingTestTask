8d8ed024a2527fc06a8fd70aea2ae2aa2ad43ddb
TimSelivan <timselivanson@gmail.com>
2023-09-13 22:59:37 +0300

    Refactor CharacterFrequencyService

--- src/main/java/com/t1consulting/service/CharacterFrequencyService.java
+++ src/main/java/com/t1consulting/service/CharacterFrequencyService.java
@@ -11,22 +11,7 @@ import java.util.stream.Collectors;
 public class CharacterFrequencyService {
 
     public String calculateCharacterFrequency(String input) {
-        Map<Character, Integer> frequencyMap = new HashMap<>();
-
-        for (char c : input.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
-        }
-
-        //sorting frequencyMap
-        LinkedHashMap<Character, Integer> sortedFrequencyMap = frequencyMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue,
-                        (e1, e2) -> e1, LinkedHashMap::new
-                ));
-
-        return sortedFrequencyMap.toString();
+        return calculateCharacterFrequencyMap(input).toString();
     }
 
     public Map<Character, Integer> calculateCharacterFrequencyMap(String input) {
